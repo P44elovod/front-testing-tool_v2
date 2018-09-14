8d1da27fe45c068466dd926637dbe2ed186a2bc8
Evgeniy Kovtun <evgeniy.ko@onyxenterprises.com>
2018-09-14 15:36:17 +0300

    filled converters

--- src/main/java/com/p44elovod/fronttestingtool/converters/UserCommandToUser.java
+++ src/main/java/com/p44elovod/fronttestingtool/converters/UserCommandToUser.java
@@ -1,4 +1,30 @@
 package com.p44elovod.fronttestingtool.converters;
 
-public class UserCommandToUser {
+import com.p44elovod.fronttestingtool.commands.UserCommand;
+import com.p44elovod.fronttestingtool.models.User;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class UserCommandToUser implements Converter<User, UserCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UserCommand convert(User source) {
+
+
+        if (source == null) {
+            return null;
+        }
+        final UserCommand userCommand = new UserCommand();
+
+        userCommand.setId(source.getId());
+        userCommand.setBirthDate(source.getBirthDate());
+        userCommand.setCellPhone(source.getCellPhone());
+        userCommand.setPersEmail(source.getPersEmail());
+        userCommand.setWorkEmail(source.getWorkEmail());
+        userCommand.setUserName(source.getUserName());
+        return userCommand;
+    }
 }
--- src/main/java/com/p44elovod/fronttestingtool/converters/UserToUserCommand.java
+++ src/main/java/com/p44elovod/fronttestingtool/converters/UserToUserCommand.java
@@ -1,4 +1,34 @@
 package com.p44elovod.fronttestingtool.converters;
 
-public class UserToUserCommand {
+import com.p44elovod.fronttestingtool.commands.UserCommand;
+import com.p44elovod.fronttestingtool.models.User;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+public class UserToUserCommand implements Converter<UserCommand, User> {
+
+
+    @Synchronized
+    @Nullable
+    @Override
+    public User convert(UserCommand source) {
+
+        if (source == null) {
+            return null;
+        }
+
+
+        final User user = new User();
+
+        user.setId(source.getId());
+        user.setUserName(source.getUserName());
+        user.setCellPhone(source.getCellPhone());
+        user.setWorkEmail(source.getWorkEmail());
+        user.setPersEmail(source.getPersEmail());
+        user.setBirthDate(source.getBirthDate());
+
+
+        return user;
+    }
 }
